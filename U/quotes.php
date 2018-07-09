3e72c25736c65276a0f9249f9db7404faf058ba3
Debashish <34054340+debashish-starshines@users.noreply.github.com>
2018-07-09 18:06:06 +0530

    Update quotes.php

--- U/quotes.php
+++ U/quotes.php
@@ -1,30 +1,59 @@
-<?php
-if (!$_POST) {}
-else{
-
-$connect = new mysqli("localhost", "id6050580_igloo", "qwerty123", "id6050580_igloo");
-if ($_FILES['csv']['size'] > 0) {
-    //get the csv file 
-    $file1 = $_FILES['csv']['tmp_name']; 
-    $handle1 = fopen($file1, "r");
-    $i = 0;
-    while (($data = fgetcsv($handle1, 1000, ",")) !== FALSE) {
-        if ($i > 0) {
-
-$import2 = "INSERT into quotes(id,name) values('$data[0]','$data[1]')";
-            $connect->query($import2);
-        }
-       
-       $i++;
-    }
-    fclose($handle1);
-    print "Import done";
-}
-else{
-    echo '<script language="javascript">';
-    echo 'alert("Select a file to upload")';
-echo '</script>';
-}
-
-}
+<?php
+
+if (!$_POST) {}
+
+else{
+
+
+
+$connect = new mysqli("us-cdbr-iron-east-04.cleardb.net", "b417e473b8c444", "35ca274c", "heroku_2de0804174260de");
+
+if ($_FILES['csv']['size'] > 0) {
+
+    //get the csv file 
+
+    $file1 = $_FILES['csv']['tmp_name']; 
+
+    $handle1 = fopen($file1, "r");
+
+    $i = 0;
+
+    while (($data = fgetcsv($handle1, 1000, ",")) !== FALSE) {
+
+        if ($i > 0) {
+
+
+
+$import2 = "INSERT into quotes(id,name) values('$data[0]','$data[1]')";
+
+            $connect->query($import2);
+
+        }
+
+       
+
+       $i++;
+
+    }
+
+    fclose($handle1);
+
+    print "Import done";
+
+}
+
+else{
+
+    echo '<script language="javascript">';
+
+    echo 'alert("Select a file to upload")';
+
+echo '</script>';
+
+}
+
+
+
+}
+
 ?>
