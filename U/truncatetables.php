414197c3477b0001c30842271dc9140353ac58a2
Debashish <34054340+debashish-starshines@users.noreply.github.com>
2018-07-13 01:31:22 +0530

    Update truncatetables.php

--- U/truncatetables.php
+++ U/truncatetables.php
@@ -1,5 +1,16 @@
 <?php
-   
+
+$dbHost     = "us-cdbr-iron-east-04.cleardb.net";
+$dbUsername = "b417e473b8c444";
+$dbPassword = "35ca274c";
+$dbName     = "heroku_2de0804174260de";
+
+$link = mysql_connect('$dbHost', '$dbUsername', '$dbPassword','$dbName');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+
     if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['TRUNCATE']))
     {
         func();
@@ -7,6 +18,7 @@
     function func()
     {
         // do stuff   
-        mysqli_query($con,'TRUNCATE TABLE images,quotes,thedaytoday');
+        mysqli_query($link,'TRUNCATE TABLE images,quotes,thedaytoday');
     }
+mysql_close($link);
 ?>
