5777e8aae6f449e377379135e2134f6cd3cfc51f
Debashish <34054340+debashish-starshines@users.noreply.github.com>
2018-07-13 01:00:19 +0530

    Create truncatetables.php

---
+++ U/truncatetables.php
@@ -0,0 +1,11 @@
+<?php
+    if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['TRUNCATE']))
+    {
+        func();
+    }
+    function func()
+    {
+        // do stuff   
+        mysqli_query($con,'TRUNCATE TABLE images,quotes,thedaytoday');
+    }
+?>
