77e3268411969d77e413acb840b41b9ac2b664b4
Debashish <34054340+debashish-starshines@users.noreply.github.com>
2018-07-19 23:50:30 +0530

    Update truncatetables_thedaytoday.php

--- U/truncatetables_thedaytoday.php
+++ U/truncatetables_thedaytoday.php
@@ -1,1 +1,21 @@
-
+<?php
+$servername = "us-cdbr-iron-east-04.cleardb.net";
+$username = "b417e473b8c444";
+$password = "35ca274c";
+$dbname = "heroku_2de0804174260de";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+// sql to truncate table
+$sql = "TRUNCATE TABLE thedaytoday";
+if ($conn->query($sql) === TRUE) {
+    echo "Tables truncated successfully";
+} else {
+    echo "Error truncating table: " . $conn->error;
+}
+$conn->close();
+ 
+?>
