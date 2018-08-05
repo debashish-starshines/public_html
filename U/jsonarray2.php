54a383e67616832bc04a2c316293764c05f8c8db
Debashish <34054340+debashish-starshines@users.noreply.github.com>
2018-08-05 09:16:38 +0530

    Update jsonarray2.php

--- U/jsonarray2.php
+++ U/jsonarray2.php
@@ -21,17 +21,17 @@
     $password="35ca274c";  
     $hostname = "us-cdbr-iron-east-04.cleardb.net";  
     //connection string with database  
-    $dbhandle = mysql_connect($hostname, $username, $password)  
+    $dbhandle = mysqli_connect($hostname, $username, $password)  
     or die("Unable to connect to MySQL");  
     echo "Connected to MySQL<br>";  
     // connect with database  
-    $selected = mysql_select_db("heroku_2de0804174260de",$dbhandle)  
+    $selected = mysqli_select_db("heroku_2de0804174260de",$dbhandle)  
     or die("Could not select database");  
     //query fire  
-    $result = mysql_query("SELECT * FROM quotes");  
+    $result = mysqli_query("SELECT * FROM quotes");  
     $json_response = array();  
     // fetch data in array format  
-    while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {  
+    while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {  
     // Fetch data of Fname Column and store in array of row_array  
     $row_array['name'] = $row['name'];  
     //push the values in the array  
@@ -39,4 +39,5 @@
     }  
     //  
     echo json_encode($json_response); 
+    mysqli_close($dbhandle);
 ?>
