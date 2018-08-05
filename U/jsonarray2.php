69de6a65bf9152bdead4072367ff7ca7050340d8
Debashish <34054340+debashish-starshines@users.noreply.github.com>
2018-08-05 08:53:22 +0530

    Update jsonarray2.php

--- U/jsonarray2.php
+++ U/jsonarray2.php
@@ -1,6 +1,6 @@
 <?php
     //open connection to mysql db
-  $connection = mysqli_connect("us-cdbr-iron-east-04.cleardb.net","b417e473b8c444","35ca274c","heroku_2de0804174260de") or die("Error " . mysqli_error($connection));
+  /* $connection = mysqli_connect("us-cdbr-iron-east-04.cleardb.net","b417e473b8c444","35ca274c","heroku_2de0804174260de") or die("Error " . mysqli_error($connection));
     //fetch table rows from mysql db
     $sql = "select * from quotes";
     $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
@@ -14,6 +14,24 @@
     //echo json_encode($emparray);
     echo json_encode(array('igloo' => $emparray)) ;
 
+    //close the db connection
+    mysqli_close($connection); */
+
+
+
+    //open connection to mysql db
+    $connection = mysqli_connect("us-cdbr-iron-east-04.cleardb.net","b417e473b8c444","35ca274c","heroku_2de0804174260de") or die("Error " . mysqli_error($connection));
+    //fetch table rows from mysql db
+    $sql = "select * from quotes ORDER BY id DESC";
+    $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+    //create an array
+    $emparray = array();
+    while($row =mysqli_fetch_assoc($result))
+    {
+        $emparray[] = $row;
+    }
+    //echo json_encode($emparray);
+    echo json_encode(array('igloo' => $emparray)) ;
     //close the db connection
     mysqli_close($connection);
 ?>
