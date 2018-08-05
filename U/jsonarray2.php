c8a16be2c91ea6ed73f02401297c0e39d6158202
Debashish <34054340+debashish-starshines@users.noreply.github.com>
2018-08-05 09:19:04 +0530

    Update jsonarray2.php

--- U/jsonarray2.php
+++ U/jsonarray2.php
@@ -1,6 +1,6 @@
 <?php
     //open connection to mysql db
-   /* $connection = mysqli_connect("us-cdbr-iron-east-04.cleardb.net","b417e473b8c444","35ca274c","heroku_2de0804174260de") or die("Error " . mysqli_error($connection));
+    $connection = mysqli_connect("us-cdbr-iron-east-04.cleardb.net","b417e473b8c444","35ca274c","heroku_2de0804174260de") or die("Error " . mysqli_error($connection));
     //fetch table rows from mysql db
     $sql = "select * from quotes";
     $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
@@ -12,32 +12,10 @@
         $emparray[] = $row;
     }
     //echo json_encode($emparray);
-    echo json_encode(array('igloo' => $emparray)) ;
+    echo json_encode(array('igloohere' => $emparray)) ;
 
     //close the db connection
-    mysqli_close($connection); */
+    mysqli_close($connection); 
 
-    $username="b417e473b8c444";  
-    $password="35ca274c";  
-    $hostname = "us-cdbr-iron-east-04.cleardb.net";  
-    //connection string with database  
-    $dbhandle = mysqli_connect($hostname, $username, $password)  
-    or die("Unable to connect to MySQL");  
-    echo "Connected to MySQL<br>";  
-    // connect with database  
-    $selected = mysqli_select_db("heroku_2de0804174260de",$dbhandle)  
-    or die("Could not select database");  
-    //query fire  
-    $result = mysqli_query("SELECT * FROM quotes");  
-    $json_response = array();  
-    // fetch data in array format  
-    while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {  
-    // Fetch data of Fname Column and store in array of row_array  
-    $row_array['name'] = $row['name'];  
-    //push the values in the array  
-    array_push($json_response,$row_array);  
-    }  
-    //  
-    echo json_encode($json_response); 
-    mysqli_close($dbhandle);
+   
 ?>
